8651330c8814b344abba945c24d890a12423581e
christoffertoppe <chris.mailbox85@gmail.com>
2020-02-22 22:53:38 +0200

    whitespace removed from beginning and end

--- src/main/java/model/SearchLogic.java
+++ src/main/java/model/SearchLogic.java
@@ -1,8 +1,12 @@
 package model;
 
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SearchLogic {
@@ -17,26 +21,44 @@ public class SearchLogic {
     public List<Restaurant> Search(List<Restaurant> restaurantList, String searchWord) {
 
 
-        List<Restaurant> copy = new ArrayList<>();
-
-        searchWord = searchWord.toUpperCase();
-        String newsearchWord = "\\S+" + searchWord;
-
-        System.out.println(searchWord);
-        System.out.println(newsearchWord);
-
-        for (Restaurant restaurant : restaurantList) {
-
-            if (restaurant.getName().contains(searchWord)) {
-            //if(Pattern.matches(searchWord, restaurant.getName())) {
-             copy.add(restaurant);
+            int indexOfFirstLetter = 0;
+            String[] stringPalat = searchWord.split("");
+            for (int i = 0; i < stringPalat.length; i++) {
+                if (stringPalat[i].matches("\\S")) {
+                    indexOfFirstLetter = i;
+                    i = stringPalat.length;
+                }
             }
-        }
-        if(searchWord.equals("")) {
-            return restaurantList;
-        }
-        return copy;
+            String newSearchWord = "";
+            for (int i = indexOfFirstLetter; i < stringPalat.length; i++) {
+                newSearchWord = newSearchWord + stringPalat[i];
+            }
 
+            int indexOfLastLetter = 0;
 
-    }
+            stringPalat = newSearchWord.split("");
+
+            for (int i = stringPalat.length - 1; i > 0; i--) {
+                if (stringPalat[i].matches("\\S")) {
+                    indexOfLastLetter = i;
+                    i = 0;
+                }
+            }
+
+            String searchWordwithoutWhitespace = "";
+            for (int i = 0; i <= indexOfLastLetter; i++) {
+                searchWordwithoutWhitespace = searchWordwithoutWhitespace + stringPalat[i];
+            }
+            if (searchWordwithoutWhitespace.equals("")) return restaurantList;
+
+            List<Restaurant> copy = new ArrayList<>();
+            searchWordwithoutWhitespace = searchWordwithoutWhitespace.toUpperCase();
+
+            for (Restaurant restaurant : restaurantList) {
+                if (restaurant.getName().contains(searchWordwithoutWhitespace)) {
+                    copy.add(restaurant);
+                }
+            }
+            return copy;
+        }
 }
