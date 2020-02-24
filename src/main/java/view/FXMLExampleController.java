ac7a4861d935920ef21d0e344134dd48fca6e200
Katri Rasio <katri.rasio@metropolia.fi>
2020-02-24 11:12:27 +0200

    google api calls & small bug fixes

--- src/main/java/view/FXMLExampleController.java
+++ src/main/java/view/FXMLExampleController.java
@@ -9,6 +9,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
@@ -16,8 +17,10 @@ import javafx.scene.layout.AnchorPane;
 import main.MainApp;
 import model.Restaurant;
 
+import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -27,6 +30,10 @@ import com.lynden.gmapsfx.MapComponentInitializedListener;
 import javafx.scene.control.Button;
 import model.SearchLogic;
 import netscape.javascript.JSObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class FXMLExampleController implements Initializable, MapComponentInitializedListener {
 
@@ -53,12 +60,22 @@ public class FXMLExampleController implements Initializable, MapComponentInitial
 
 	@FXML
 	private TextField searchTextBox;
+	@FXML private CheckBox checkBox;
+
 
 	@FXML
 	public void handleSearchBar(KeyEvent keyEvent) {
 		String textInSearchField = searchTextBox.getText();
 		List<Restaurant> foundRestaurants = search.Search(mainApp.getRestaurants(), textInSearchField);
 		updateListView(foundRestaurants);
+	}
+
+	@FXML protected void handleCheckbox(ActionEvent event) {
+		String textInSearchField = searchTextBox.getText();
+		if (checkBox.isSelected()) {
+			LatLong ll = fetchNormalJava(textInSearchField);
+			focusMapOnCoordinate(ll);
+		}
 	}
 
 	@FXML
@@ -111,17 +128,20 @@ public class FXMLExampleController implements Initializable, MapComponentInitial
 		System.out.println("UPDATE IN PROGRESS FOR MAPS");
 		listViewNames.getItems().clear();
 		List<Marker> restaurantMarkers = new ArrayList<>();
+		map.clearMarkers();
 
 		listViewNames.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-			Restaurant restaurantToFind = new Restaurant();
-			System.out.println(newValue);
-			for (int i = 0; i < restaurants.size(); i++) {
-				if (restaurants.get(i).getName().equals(newValue)) {
-					restaurantToFind = restaurants.get(i);
-					break;
+			if (newValue != null) {
+				Restaurant restaurantToFind = new Restaurant();
+				System.out.println(newValue);
+				for (int i = 0; i < restaurants.size(); i++) {
+					if (restaurants.get(i).getName().equals(newValue)) {
+						restaurantToFind = restaurants.get(i);
+						break;
+					}
 				}
+				showRestaurantDetails(restaurantToFind);
 			}
-			showRestaurantDetails(restaurantToFind);
 		});
 
 		// Lisätään ravintoloiden nimet ObservableListiin
@@ -160,4 +180,67 @@ public class FXMLExampleController implements Initializable, MapComponentInitial
 		}
 	}
 
+	public LatLong fetchNormalJava(String s) {
+		System.out.println("FetchJava alku");
+		String sWithoutSpaces = s
+				.replace(",", "")
+				.replace(" ", "+");
+		String httpsUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="+ sWithoutSpaces +
+				"&key=" + api;
+		URL url;
+		HttpsURLConnection con = null;
+		StringBuilder result = new StringBuilder();
+		InputStream in = null;
+		BufferedReader reader = null;
+
+		try {
+			url = new URL(httpsUrl);
+			con = (HttpsURLConnection)url.openConnection();
+			in = new BufferedInputStream(con.getInputStream());
+			reader = new BufferedReader(new InputStreamReader(in));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				result.append(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				in.close();
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+			}
+			if (con != null) {
+				con.disconnect();
+			}
+		}
+
+		LatLong ll = null;
+		JSONObject resultJSON = new JSONObject(result.toString());
+		JSONArray resultArray = resultJSON.getJSONArray("results");
+		for (int i = 0; i < resultArray.length(); i++) {
+			JSONObject results = resultArray.getJSONObject(i);
+			JSONObject geometry = results.getJSONObject("geometry");
+			JSONObject location = geometry.getJSONObject("location");
+			ll = new LatLong(location.getDouble("lat"), location.getDouble("lng"));
+		}
+		System.out.println("fetch loppu");
+		return ll;
+	}
+
+	private void focusMapOnCoordinate(LatLong ll) {
+		System.out.printf("focusOnCoordinate alku");
+		MarkerOptions markerOptions = new MarkerOptions();
+		markerOptions.position(ll);
+		// TODO tähän markerille uusi ulkonäkö
+		markerOptions.label("Hakemasi osoite");
+		Marker tempMarker = new Marker(markerOptions);
+		map.addMarkers(Collections.singletonList(tempMarker));
+		mapView.setCenter(ll.getLatitude(), ll.getLongitude());
+		mapView.setZoom(15);
+		System.out.println("focusOnCoordinate loppu");
+	}
 }
