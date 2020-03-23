544c6a08ba364c3a0d6bcdf8ded39ea1b2030598
Katri Rasio <katri.rasio@metropolia.fi>
2020-03-23 14:47:26 +0200

    fix Oulun yliopistollinen sairaala-address & coordinates

--- src/main/java/main/Update.java
+++ src/main/java/main/Update.java
@@ -46,9 +46,24 @@ public class Update {
 
     }
 
+    public static boolean updateRestaurantData() {
+        boolean success = false;
+        //      "lat": 65.007202,
+        //      "lng": 25.518100
 
-    public static void main(String args[]) throws Exception  //static method
-    {
-        readJSON();
+        RestaurantDAO dao = new RestaurantDAO();
+        Restaurant restaurant = new Restaurant(142, 65.007202, 25.518100);
+        restaurant.setAddress("Kajaanintie 50");
+        restaurant.setPostal_code("90220");
+        restaurant.setCity("Oulu");
+        success = dao.updateRestaurant(restaurant);
+
+        return success;
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        System.out.println(updateRestaurantData());
+//        readJSON();
     }
 }
