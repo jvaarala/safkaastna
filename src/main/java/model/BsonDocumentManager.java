076686d4a756b12fa925576716682e46fa15de8e
OliverMartikainen <48790588+OliverMartikainen@users.noreply.github.com>
2020-05-03 20:51:35 +0300

    remove duplicate code

--- src/main/java/model/BsonDocumentManager.java
+++ src/main/java/model/BsonDocumentManager.java
@@ -1,8 +1,5 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bson.Document;
 
 public abstract class BsonDocumentManager {
@@ -36,16 +33,7 @@ public abstract class BsonDocumentManager {
 	 */
 	protected Restaurant docToRestaurant(Document doc) {
 		Restaurant res = new Restaurant();
-		res.setId(doc.getInteger("id"));
-		res.setName(doc.getString("name"));
-		res.setAddress(doc.getString("address"));
-		res.setPostal_code(doc.getString("postal_code"));
-		res.setCity(doc.getString("city"));
-		res.setWww(doc.getString("www"));
-		res.setAdmin(doc.getString("admin"));
-		res.setAdmin_www(doc.getString("admin_www"));
-		res.setLat(doc.getDouble("lat"));
-		res.setLng(doc.getDouble("lng"));
+		setRestaurantFromDoc(doc, res);
 		return res;
 	}
 	
@@ -66,41 +54,5 @@ public abstract class BsonDocumentManager {
 		res.setAdmin_www(doc.getString("admin_www"));
 		res.setLat(doc.getDouble("lat"));
 		res.setLng(doc.getDouble("lng"));
-	}
-	
-	/**
-	 * Uses BSON document class to parse its toJson output into documents
-	 * Then returns the documents as restaurant objects
-	 * @param List<String> BSON document toJson output strings list
-	 * @return List<Restaurant> normal restaurant array
-	 */
-	public List<Restaurant> createRestaurantsList(List<String> restaurantJsonStrings) {
-		List<Restaurant> restaurants = new ArrayList<Restaurant>();
-		for(String s : restaurantJsonStrings) {
-			Document restaurantDoc = Document.parse(s);
-			Restaurant r = docToRestaurant(restaurantDoc);
-			
-			restaurants.add(r);
-		}
-
-		return restaurants;
-	}
-	
-	
-	/**
-	 * Uses BSON document classes toJson ability to create json strings
-	 * 
-	 * @param List<Restaurant>
-	 * @return List<String> restaurants in json string
-	 */
-	public List<String> createRestaurantsJson(List<Restaurant> restaurants) {
-		List<String> restaurantsJSON = new ArrayList<String>();
-
-		for (Restaurant res : restaurants) {
-			Document restaurantDoc = restaurantToDoc(res);
-			restaurantsJSON.add(restaurantDoc.toJson());
-		}
-
-		return restaurantsJSON;
 	}
 }
